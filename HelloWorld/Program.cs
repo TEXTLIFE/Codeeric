6aa844cac112d7702475a084575cb995c70602aa
Enrique Tan <eric@Enriques-MacBook-Air.local>
2025-05-01 16:09:57 +0800

    first C# commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a variable
+            string greeting = "Hello, World!";
+            
+            // Print the variable to the console
+            Console.WriteLine(greeting);
+        }
+    }
+}
